ad6273059d7c6b2b1042d46b8f66cda7eaf48d4c
mob35 <toonelicious@gmail.com>
2017-07-25 17:32:58 +0700

    dook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make WalkthroughPage the root (or first) page
-  rootPage: any = ContactCardPage;
+  rootPage: any = WalkthroughPage;
 
   textDir: string = "ltr";
 
